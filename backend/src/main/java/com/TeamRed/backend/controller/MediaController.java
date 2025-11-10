2aee8139bd49f3099d2d0f8ad16e2db50b044b6c
tbeat004 <167821818+tbeat004@users.noreply.github.com>
2025-11-10 03:11:18 -0500

    Refactor MediaController and TmdbClient for generic TMDB API calls
    Refactored MediaController to use a generic get method in TmdbClient for all TMDB API calls, reducing code duplication and improving maintainability. Added a helper method to build query parameters dynamically. Updated endpoints to match TMDB API paths.

--- backend/src/main/java/com/TeamRed/backend/controller/MediaController.java
+++ backend/src/main/java/com/TeamRed/backend/controller/MediaController.java
@@ -6,7 +6,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.HashMap;
+import java.util.Map;
 
 @Tag(
         name = "Media",
@@ -15,7 +16,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/media")
 @CrossOrigin(origins = "*")
-
 public class MediaController {
 
     private final TmdbClient tmdbClient;
@@ -23,83 +23,193 @@ public class MediaController {
     public MediaController(TmdbClient tmdbClient) {
         this.tmdbClient = tmdbClient;
     }
+
+    // ================= SEARCH =================
+
     @Operation(
             summary = "Search Multi",
             description = "Searches movies, TV shows, and people."
     )
-    @GetMapping("/search/all")
-    public ResponseEntity<Object> searchAll(@RequestParam String query, @RequestParam(required = false) Boolean includeAdult, @RequestParam(required = false) String language, @RequestParam(required = false) Integer page) {
-        return ResponseEntity.ok(tmdbClient.searchMulti(query, includeAdult, language, page));
+    @GetMapping("/search/multi")
+    public ResponseEntity<Object> searchMulti(
+            @RequestParam String query,
+            @RequestParam(required = false) Boolean includeAdult,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) Integer page) {
+
+        String endpoint = "/search/multi";
+        Map<String, Object> params = buildParams(
+                "query", query,
+                "include_adult", includeAdult,
+                "language", language,
+                "page", page
+        );
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
 
     @Operation(
             summary = "Search for movies",
-            description = "Searches for movies by title and some optional filters.")
+            description = "Searches for movies by title and optional filters."
+    )
     @GetMapping("/search/movies")
-    public ResponseEntity<Object> searchMovies(@RequestParam String query, @RequestParam(required = false) Boolean includeAdult,
-                                               @RequestParam(required = false) Integer page,
-                                               @RequestParam(required = false) String language,
-                                               @RequestParam(required = false) String region,
-                                               @RequestParam(required = false) String year,
-                                               @RequestParam(required = false) String primaryReleaseYear) {
-        return ResponseEntity.ok(tmdbClient.searchMovies(query, includeAdult, page, language, region, year, primaryReleaseYear));
+    public ResponseEntity<Object> searchMovies(
+            @RequestParam String query,
+            @RequestParam(required = false) Boolean includeAdult,
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) String region,
+            @RequestParam(required = false) String year,
+            @RequestParam(required = false, name = "primary_release_year") String primaryReleaseYear) {
+
+        String endpoint = "/search/movie";
+        Map<String, Object> params = buildParams(
+                "query", query,
+                "include_adult", includeAdult,
+                "language", language,
+                "page", page,
+                "region", region,
+                "year", year,
+                "primary_release_year", primaryReleaseYear
+        );
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
+
+    // ================= MOVIE CALLS =================
 
     @Operation(
             summary = "Now Playing",
-            description = "Retrieves movies that are currently in theatres")
+            description = "Retrieves movies that are currently in theatres."
+    )
     @GetMapping("/movies/now_playing")
-    public ResponseEntity<Object> getNowPlaying(@RequestParam(required = false) Integer page,
-                                                   @RequestParam(required = false) String language,
-                                                   @RequestParam(required = false) String region) {
-        return ResponseEntity.ok(tmdbClient.getNowPlaying(page, language, region));
+    public ResponseEntity<Object> getNowPlayingMovies(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) String region) {
+
+        String endpoint = "/movie/now_playing";
+        Map<String, Object> params = buildParams(
+                "page", page,
+                "language", language,
+                "region", region
+        );
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
 
     @Operation(
             summary = "Popular",
-            description = "Retrieves popular movies.")
+            description = "Retrieves popular movies."
+    )
     @GetMapping("/movies/popular")
-    public ResponseEntity<Object> getPopularMovies(@RequestParam(required = false) Integer page,
-                                                   @RequestParam(required = false) String language,
-                                                   @RequestParam(required = false) String region) {
-        return ResponseEntity.ok(tmdbClient.getPopularMovies(page, language, region));
+    public ResponseEntity<Object> getPopularMovies(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) String region) {
+
+        String endpoint = "/movie/popular";
+        Map<String, Object> params = buildParams(
+                "page", page,
+                "language", language,
+                "region", region
+        );
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
 
     @Operation(
             summary = "Top Rated",
-            description = "Retrieves top rated movies.")
+            description = "Retrieves top-rated movies."
+    )
     @GetMapping("/movies/top_rated")
-    public ResponseEntity<Object> getTopRated(@RequestParam(required = false) Integer page,
-                                                   @RequestParam(required = false) String language,
-                                                   @RequestParam(required = false) String region) {
-        return ResponseEntity.ok(tmdbClient.getTopRated(page, language, region));
+    public ResponseEntity<Object> getTopRatedMovies(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) String region) {
+
+        String endpoint = "/movie/top_rated";
+        Map<String, Object> params = buildParams(
+                "page", page,
+                "language", language,
+                "region", region
+        );
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
 
     @Operation(
             summary = "Upcoming",
-            description = "Retrieves upcoming movies.")
+            description = "Retrieves upcoming movies."
+    )
     @GetMapping("/movies/upcoming")
-    public ResponseEntity<Object> getUpcoming(@RequestParam(required = false) Integer page,
-                                                   @RequestParam(required = false) String language,
-                                                   @RequestParam(required = false) String region) {
-        return ResponseEntity.ok(tmdbClient.getUpcoming(page, language, region));
+    public ResponseEntity<Object> getUpcomingMovies(
+            @RequestParam(required = false) Integer page,
+            @RequestParam(required = false) String language,
+            @RequestParam(required = false) String region) {
+
+        String endpoint = "/movie/upcoming";
+        Map<String, Object> params = buildParams(
+                "page", page,
+                "language", language,
+                "region", region
+        );
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
 
     @Operation(
             summary = "Get movie details",
-            description = "Retrieves detailed information about a specific movie by its TMDB ID.")
+            description = "Retrieves detailed information about a specific movie by its TMDB ID."
+    )
     @GetMapping("/movies/{tmdbId}")
-    public ResponseEntity<Object> getMovieDetails(@PathVariable long tmdbId,
-                                                  @RequestParam(required = false) String language) {
-        return ResponseEntity.ok(tmdbClient.getMovieDetails(tmdbId, language));
+    public ResponseEntity<Object> getMovieDetails(
+            @PathVariable long tmdbId,
+            @RequestParam(required = false) String language) {
+
+        String endpoint = "/movie/" + tmdbId;
+        Map<String, Object> params = buildParams("language", language);
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
     }
 
     @Operation(
             summary = "Get movie credits",
-            description = "Retrieves detailed information about the cast and crew of a specific movie by its TMDB ID.")
+            description = "Retrieves cast and crew information for a specific movie."
+    )
     @GetMapping("/movies/{tmdbId}/credits")
-    public ResponseEntity<Object> getMovieCredits(@PathVariable long tmdbId,
-                                                  @RequestParam(required = false) String language) {
-        return ResponseEntity.ok(tmdbClient.getMovieCredits(tmdbId, language));
+    public ResponseEntity<Object> getMovieCredits(
+            @PathVariable long tmdbId,
+            @RequestParam(required = false) String language) {
+
+        String endpoint = "/movie/" + tmdbId + "/credits";
+        Map<String, Object> params = buildParams("language", language);
+
+        return ResponseEntity.ok(tmdbClient.get(endpoint, params));
+    }
+
+    @Operation(
+            summary = "Get movie watch providers",
+            description = "Returns a list of watch providers for a movie."
+    )
+    @GetMapping("/movies/{tmdbId}/watch/providers")
+    public ResponseEntity<Object> getMovieWatchProviders(@PathVariable long tmdbId) {
+
+        String endpoint = "/movie/" + tmdbId + "/watch/providers";
+        return ResponseEntity.ok(tmdbClient.get(endpoint, new HashMap<>()));
+    }
+
+    // ================= HELPER =================
+
+    private Map<String, Object> buildParams(Object... keyValuePairs) {
+        Map<String, Object> map = new HashMap<>();
+        for (int i = 0; i < keyValuePairs.length; i += 2) {
+            String key = (String) keyValuePairs[i];
+            Object value = keyValuePairs[i + 1];
+            if (value != null && !value.toString().isBlank()) {
+                map.put(key, value);
+            }
+        }
+        return map;
     }
 }
--- backend/src/main/java/com/TeamRed/backend/external/tmdb/TmdbClient.java
+++ backend/src/main/java/com/TeamRed/backend/external/tmdb/TmdbClient.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.util.Map;
 import java.util.Optional;
 
 @Component
@@ -18,114 +19,198 @@ public class TmdbClient {
 
     private final RestTemplate restTemplate = new RestTemplate();
 
+    public Object get(String endpoint, Map<String, Object> params) {
+        UriComponentsBuilder builder = UriComponentsBuilder
+                .fromUriString(baseUrl + endpoint)
+                .queryParam("api_key", apiKey);
 
-    // MOVIE CALLS
+        if (params != null) {
+            params.forEach(builder::queryParam);
+        }
 
-    public Object getNowPlaying(Integer page, String language, String region) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/now_playing")
-                .queryParam("api_key", apiKey)
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .queryParamIfPresent("page", Optional.ofNullable(page))
-                .queryParamIfPresent("region", Optional.ofNullable(region))
-                .toUriString();
-        return fetch(url);
+        return restTemplate.getForObject(builder.toUriString(), Object.class);
     }
-
-
-    public Object getPopularMovies(Integer page, String language, String region) {
-        String url = UriComponentsBuilder
-                .fromUriString(baseUrl + "/movie/popular")
-                .queryParam("api_key", apiKey)
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .queryParamIfPresent("page", Optional.ofNullable(page))
-                .queryParamIfPresent("region", Optional.ofNullable(region))
-                .toUriString();
-
-        return fetch(url);
-    }
-
-    public Object getTopRated(Integer page, String language, String region) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/top_rated")
-                .queryParam("api_key", apiKey)
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .queryParamIfPresent("page", Optional.ofNullable(page))
-                .queryParamIfPresent("region", Optional.ofNullable(region))
-                .toUriString();
-        return fetch(url);
-    }
-
-    public Object getUpcoming(Integer page, String language, String region) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/upcoming")
-                .queryParam("api_key", apiKey)
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .queryParamIfPresent("page", Optional.ofNullable(page))
-                .queryParamIfPresent("region", Optional.ofNullable(region))
-                .toUriString();
-
-        return fetch(url);
-    }
-
-
-
-    public Object searchMovies(String query, Boolean includeAdult, Integer page, String language, String region, String year, String primaryReleaseYear) {
-
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/search/movie")
-                .queryParam("api_key", apiKey)
-                .queryParam("query", query)
-                .queryParamIfPresent("include_adult", Optional.ofNullable(includeAdult))
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .queryParamIfPresent("page", Optional.ofNullable(page))
-                .queryParamIfPresent("region", Optional.ofNullable(region))
-                .queryParamIfPresent("year", Optional.ofNullable(year))
-                .queryParamIfPresent("primary_release_year", Optional.ofNullable(primaryReleaseYear))
-                .toUriString();
-
-        return fetch(url);
-    }
-
-
-    public Object getMovieDetails(long tmdbId, String language) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + tmdbId)
-                .queryParam("api_key", apiKey)
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .toUriString();
-
-        return fetch(url);
-    }
-
-    public Object getMovieCredits(long tmdbId, String language) {
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + tmdbId + "/credits")
-                .queryParam("api_key", apiKey)
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .toUriString();
-
-        return fetch(url);
-    }
-
-
-    // TV SHOW CALLS
-
-    // ACTOR CALLS
-
-    // MULTI CALLS
-
-    public Object searchMulti(String query, Boolean includeAdult, String language, Integer page) {
-
-        String url = UriComponentsBuilder.fromUriString(baseUrl + "/search/multi")
-                .queryParam("api_key", apiKey)
-                .queryParam("query", query)
-                .queryParamIfPresent("include_adult", Optional.ofNullable(includeAdult))
-                .queryParamIfPresent("language", Optional.ofNullable(language))
-                .queryParamIfPresent("page", Optional.ofNullable(page))
-                .toUriString();
-
-        return fetch(url);
-    }
-
-
-    // HELPERS
-    private Object fetch(String url) {
-        return restTemplate.getForObject(url, Object.class);
-    }
+//    // MOVIE CALLS
+//
+//    public Object getNowPlayingMovies(Integer page, String language, String region) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/now_playing")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("region", Optional.ofNullable(region))
+//                .toUriString();
+//        return fetch(url);
+//    }
+//
+//
+//    public Object getPopularMovies(Integer page, String language, String region) {
+//        String url = UriComponentsBuilder
+//                .fromUriString(baseUrl + "/movie/popular")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("region", Optional.ofNullable(region))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//    public Object getTopRatedMovies(Integer page, String language, String region) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/top_rated")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("region", Optional.ofNullable(region))
+//                .toUriString();
+//        return fetch(url);
+//    }
+//
+//    public Object getUpcomingMovies(Integer page, String language, String region) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/upcoming")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("region", Optional.ofNullable(region))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//
+//
+//    public Object searchMovies(String query, Boolean includeAdult, Integer page, String language, String region, String year, String primaryReleaseYear) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/search/movie")
+//                .queryParam("api_key", apiKey)
+//                .queryParam("query", query)
+//                .queryParamIfPresent("include_adult", Optional.ofNullable(includeAdult))
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("region", Optional.ofNullable(region))
+//                .queryParamIfPresent("year", Optional.ofNullable(year))
+//                .queryParamIfPresent("primary_release_year", Optional.ofNullable(primaryReleaseYear))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//
+//    public Object getMovieDetails(long tmdbId, String language) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + tmdbId)
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//    public Object getMovieCredits(long tmdbId, String language) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + tmdbId + "/credits")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//    public Object getMovieWatchProviders(long tmdbId) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/movie/" + tmdbId + "/watch/providers")
+//                .queryParam("api_key", apiKey)
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//
+//    // TV SERIES CALLS
+//
+//    public Object getAiringTodaySeries(Integer page, String language, String timezone) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/airing_today")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("timezone", Optional.ofNullable(timezone))
+//                .toUriString();
+//        return fetch(url);
+//    }
+//
+//    public Object getOnTheAirSeries(Integer page, String language, String timezone) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/on_the_air")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .queryParamIfPresent("timezone", Optional.ofNullable(timezone))
+//                .toUriString();
+//        return fetch(url);
+//    }
+//
+//    public Object getPopularSeries(Integer page, String language) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/popular")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .toUriString();
+//        return fetch(url);
+//    }
+//
+//    public Object getTopRatedSeries(Integer page, String language) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/top_rated")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .toUriString();
+//        return fetch(url);
+//    }
+//
+//    public Object getSeriesDetails(long tmdbId, String language) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/" + tmdbId)
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//    public Object getSeriesAggregateCredits(long tmdbId, String language) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/" + tmdbId + "/aggregate_credits")
+//                .queryParam("api_key", apiKey)
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//    public Object getSeriesWatchProviders(long tmdbId) {
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/tv/" + tmdbId + "/watch/providers")
+//                .queryParam("api_key", apiKey)
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//
+//
+//
+//    // ACTOR CALLS
+//
+//    // MULTI CALLS
+//
+//    public Object searchMulti(String query, Boolean includeAdult, String language, Integer page) {
+//
+//        String url = UriComponentsBuilder.fromUriString(baseUrl + "/search/multi")
+//                .queryParam("api_key", apiKey)
+//                .queryParam("query", query)
+//                .queryParamIfPresent("include_adult", Optional.ofNullable(includeAdult))
+//                .queryParamIfPresent("language", Optional.ofNullable(language))
+//                .queryParamIfPresent("page", Optional.ofNullable(page))
+//                .toUriString();
+//
+//        return fetch(url);
+//    }
+//
+//
+//    // HELPERS
+//    private Object fetch(String url) {
+//        return restTemplate.getForObject(url, Object.class);
+//    }
 
 }
